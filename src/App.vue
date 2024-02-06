7f7947effcf6167caa08f23ee72fabe774b8716f
MatthieuMarchand <124369459+MatthieuMarchand@users.noreply.github.com>
2024-02-06 18:15:07 +0100

    Revert "Page7 painting page"

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <template>
-<!--  <LoadingPage/>-->
-<!--  <HomePage/>-->
-  <PaintingPage/>
+  <LoadingPage/>
+  <HomePage/>
 </template>
 
 <script setup>
-  // import LoadingPage from '@/components/LoadingPage.vue';
-  // import HomePage from '@/components/Homepage.vue';
-  import PaintingPage from "@/components/PaintingPage.vue";
-
+  import LoadingPage from '@/components/LoadingPage.vue';
+  import HomePage from '@/components/Homepage.vue';
 </script>
 
 <style lang="scss">
