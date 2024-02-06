3bac034034f58da68cdab98e81909ad1fcd64a64
Safirl <loic.leforestier@mmibordeaux.com>
2024-02-06 14:50:44 +0100

    Adding painting page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
-  <LoadingPage/>
-  <HomePage/>
+<!--  <LoadingPage/>-->
+<!--  <HomePage/>-->
+  <PaintingPage/>
 </template>
 
 <script setup>
-  import LoadingPage from '@/components/LoadingPage.vue';
-  import HomePage from '@/components/Homepage.vue';
+  // import LoadingPage from '@/components/LoadingPage.vue';
+  // import HomePage from '@/components/Homepage.vue';
+  import HomePage from '@/components/PaintingPage.vue';
+  import PaintingPage from "@/components/PaintingPage.vue";
+
 </script>
 
 <style lang="scss">
---
+++ src/components/PaintingPage.vue
@@ -0,0 +1,21 @@
+<template>
+<main id="main_PaintingPage">
+  <img src="/images/painting1.webp" alt="Les deux fillettes">
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem minus nostrum optio! Ad amet assumenda dolores doloribus ea hic itaque magnam nemo perferendis sit, ut velit vitae voluptas voluptate voluptatibus.
+  </p>
+
+</main>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+  #main_PaintingPage{
+    img{
+      width: 100%;
+    }
+  }
+</style>
