19913fb0b99e80908017dbc65192906dc00fdac2
Safirl <loic.leforestier@mmibordeaux.com>
2024-02-06 11:55:15 +0100

    Adding code pag and dialogue bubble

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <LoadingPage/>
-  <HomePage/>
+  <!-- <LoadingPage/> -->
+  <!--<HomePage/> -->
+  <Codepage/>
 </template>
 
 <script setup>
-  import LoadingPage from '@/components/LoadingPage.vue';
-  import HomePage from '@/components/Homepage.vue';
+  // import LoadingPage from '@/components/LoadingPage.vue';
+  // import HomePage from '@/components/Homepage.vue';
+  import Codepage from '@/components/Codepage.vue';
 </script>
 
 <style lang="scss">
